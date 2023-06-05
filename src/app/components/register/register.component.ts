b165b8f76f609a931f43b74d4c33acc20c22edcc
codeXplorerePo <codexplore.vuchinam97@gmail.com>
2023-06-05 19:15:15 +0700

    first commit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -64,7 +64,7 @@ export class RegisterComponent implements OnInit {
       .subscribe(() => {
         alert('Sign Up Successfully');
         this.signUpForm.reset();
-        this.router.navigate(['/login']);
+        this.router.navigate(['/home']);
       });
   }
 
